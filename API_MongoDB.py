bc6abf4a9694be6d0d61824e471d36f634d0be75
Tomasz Janicki <tomoicki@gmail.com>
2021-08-26 11:05:58 +0200

    plus basic Mongo api

--- API_MongoDB.py
+++ API_MongoDB.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from bson.json_util import dumps
+import pandas
 from os import environ as env
 from dotenv import load_dotenv
 from utilities.MongoDB_connection_functions import connection_to_mongodb
@@ -16,8 +16,10 @@ def get_users():
     #  create connection with MongoDB
     collection = connection_to_mongodb(env['mongoDB_host'], env['mongoDB_port'],
                                        env['mongoDB_db_name'], env['mongoDB_collection_name'])
-    retrieved_users = dumps(collection.find())
-    return retrieved_users
+    data = pandas.DataFrame(list(collection.find()))
+    data.drop(columns=['_id'], inplace=True)
+    data = data.to_dict("records")
+    return {'data': data}
 
 
 if __name__ == '__main__':
--- API_PostgreSQL.py
+++ API_PostgreSQL.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 from sqlalchemy import inspect
-from sqlalchemy.orm import sessionmaker
 import pandas
 from os import environ as env
 from dotenv import load_dotenv
@@ -12,9 +11,6 @@ load_dotenv()
 #  make connection with PostgreSQL
 cnx = connection2db(env['PostgreSQL_host2'], env['PostgreSQL_port2'], env['PostgreSQL_user2'],
                     env['PostgreSQL_password2'], env['PostgreSQL_db_name2'])
-#  create session
-Session = sessionmaker(bind=cnx)
-s = Session()
 
 
 app = Flask(__name__)
@@ -44,9 +40,8 @@ def info():
     <div>For example to execute "SELECT * FROM JobOffer WHERE expired = true" your link would need to look like:</div>
     <div>/showtable?table=JobOffer&key1=expired&operator1==&value1=true</div>
     <div>You can also add columns=column_name for query to be "SELECT column_name" instead of "SELECT *"</div>
-    <div>Example for query "SELECT title FROM JobOffer WHERE expired = true"</div>
-    <div>/showtable?table=JobOffer&columns=title&key1=expired&operator1==&value1=true</div>
-    <div>Unfortunately, in query with WHERE you can only provide 1 column_name or * for all columns</div>
+    <div>Example for query "SELECT id,title,expired FROM JobOffer WHERE expired = true"</div>
+    <div>/showtable?table=JobOffer&columns=id,title,expired&key1=expired&operator1==&value1=true</div>
     <div>WHERE query can take up to three sets of key,operator,value.</div>
     <div>For example to get only title of those job offers that have b2b_min>0 and permanent_min>0 and mandate_min>0</div>
     <div>which translates into "SELECT title FROM JobOffer WHERE b2b_min>0 and permanent_min>0 and mandate_min>0"</div>
@@ -102,28 +97,14 @@ def show_table():
             query = pandas.read_sql_table(table_name, con=cnx, columns=columns).to_dict("records")
             return {'all offers': [{f'{table_name}': line} for line in query]}
         elif key1 and operator1 and value1:
-            print("just key, operator, value")
             sql_txt = f"SELECT * FROM \"{table_name}\" WHERE \"{table_name}\".{key1}{operator1}%s"
             query = pandas.read_sql_query(sql_txt, con=cnx, params=(value1,)).to_dict("records")
             return {'all offers': [{f'{table_name}': line} for line in query]}
-        else:
+        elif table_name:
             query = pandas.read_sql_table(table_name, con=cnx).to_dict("records")
             return {'all offers': [{f'{table_name}': line} for line in query]}
-
-
-    # column = request.args.get('column', None)
-    # row = request.args.get('row', None)
-    # if tablee:
-    #     df = pandas.read_sql(tablee, cnx)
-    #     if column is None and row is None:
-    #         out = df.to_dict("records")
-    #     elif column is None and row is not None:
-    #         out = df.loc[int(row), :].to_json(orient='index')
-    #     elif column is not None and row is None:
-    #         out = df[column].to_json(orient='index')
-    #     else:
-    #         out = df.loc[int(row), column]
-    # return out
+        else:
+            return {'message': 'something went wrong'}
 
 
 if __name__ == '__main__':
